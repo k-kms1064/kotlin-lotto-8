3008778027f5c6dc1d6a10ce85edffee48a5b3e4
k-kms1064 <kms106418@gmail.com>
2025-10-29 20:57:50 +0900

    test: Lotto getNumbers 정렬 기능 테스트 추가

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
@@ -24,5 +25,11 @@ class LottoTest {
         assertThatThrownBy { Lotto(listOf(0, 2, 3, 4, 5, 6)) }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("[ERROR]")
+    }
+
+    @Test
+    fun `getNumbers는 오름차순으로 정렬된 번호를 반환한다`() {
+        val lotto = Lotto(listOf(8, 3, 6, 1, 2, 7))
+        assertThat(lotto.getNumbers()).isEqualTo(listOf(1, 2, 3, 6, 7, 8))
     }
 }
