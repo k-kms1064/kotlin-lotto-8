3a648b58f941664ec5fcece284fff18a7443c230
k-kms1064 <kms106418@gmail.com>
2025-10-29 20:42:16 +0900

    feat: Lotto 번호 중복 검증 추가

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,5 +3,6 @@ package lotto.domain
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.distinct().size == 6) { "[ERROR] 로또 번호는 중복될 수 없습니다." }
     }
 }
