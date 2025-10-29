b9a8a38d4d6e88339281e4fc7ee615fc39e90775
k-kms1064 <kms106418@gmail.com>
2025-10-29 20:55:52 +0900

    test: 로또 번호(6개 미안, 중복, 범위) 검증 예외 발생 테스트 추가

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,11 +1,28 @@
 package lotto.domain
 
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class LottoTest {
 
     @Test
-    fun `샘플 테스트`() {
+    fun `로또 번호가 6개가 아니면 예외 발생`() {
+        assertThatThrownBy { Lotto(listOf(1, 2, 3, 4, 5)) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("[ERROR]")
+    }
 
+    @Test
+    fun `로또 번호에 중복이 있으면 예외 발생`() {
+        assertThatThrownBy { Lotto(listOf(1, 2, 2, 3, 4, 5)) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("[ERROR]")
+    }
+
+    @Test
+    fun `로또 번호가 1부터 45 사이가 아니면 예외 발생`() {
+        assertThatThrownBy { Lotto(listOf(0, 2, 3, 4, 5, 6)) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("[ERROR]")
     }
 }
