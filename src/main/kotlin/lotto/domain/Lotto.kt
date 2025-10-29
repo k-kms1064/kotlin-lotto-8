8fdebff3710c031e12501e9a70a1eb3aa7a5659b
k-kms1064 <kms106418@gmail.com>
2025-10-29 20:39:34 +0900

    feat: Lotto 번호 개수 검증 추가

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,3 +1,7 @@
 package lotto.domain
 
-class Lotto(private val numbers: List<Int>)
+class Lotto(private val numbers: List<Int>) {
+    init {
+        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+    }
+}
