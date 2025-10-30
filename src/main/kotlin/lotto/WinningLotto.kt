0a57fcce71170647444a3781cc42673601307782
k-kms1064 <kms106418@gmail.com>
2025-10-30 23:32:58 +0900

    feat: WinningLotto 보너스 번호 유효성 검증 추가

--- src/main/kotlin/lotto/WinningLotto.kt
+++ src/main/kotlin/lotto/WinningLotto.kt
@@ -3,4 +3,10 @@ package lotto
 class WinningLotto(
     private val winningNumbers: Lotto,
     private val bonusNumber: Int
-)
+) {
+
+    init {
+        require(bonusNumber in 1..45) { "[ERROR] 보너스 번호는 1부터 45 사이여야 합니다." }
+        require(!winningNumbers.getNumbers().contains(bonusNumber)) { "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다." }
+    }
+}
