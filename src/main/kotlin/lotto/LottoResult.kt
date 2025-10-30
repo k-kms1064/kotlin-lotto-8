a899d98e994debef2299dd44914e4b2df03f205f
k-kms1064 <kms106418@gmail.com>
2025-10-31 00:03:54 +0900

    feat: 로또 리스트 비교 및 등수별 집계 함수 추가

--- src/main/kotlin/lotto/LottoResult.kt
+++ src/main/kotlin/lotto/LottoResult.kt
@@ -4,6 +4,19 @@ class LottoResult(
     private val results: Map<Rank, Int>
 ) {
 
+    companion object {
+        fun from(lottos: List<Lotto>, winningLotto: WinningLotto): LottoResult {
+            val resultMap = mutableMapOf<Rank, Int>()
+
+            lottos.forEach { lotto ->
+                val rank = winningLotto.match(lotto)
+                resultMap[rank] = resultMap.getOrDefault(rank, 0) + 1
+            }
+
+            return LottoResult(resultMap)
+        }
+    }
+
     fun getCount(rank: Rank): Int {
         return results.getOrDefault(rank, 0)
     }
