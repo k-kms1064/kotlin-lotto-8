3b4d74ae998fd850a20b859e0189eaf12467ffd9
k-kms1064 <kms106418@gmail.com>
2025-10-30 23:25:05 +0900

    feat: Rank 등수 판별 로직 추가

--- src/main/kotlin/lotto/Rank.kt
+++ src/main/kotlin/lotto/Rank.kt
@@ -6,5 +6,18 @@ enum class Rank(val matchCount: Int, val reward: Int) {
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
-    NONE(0, 0)
+    NONE(0, 0);
+
+    companion object {
+        fun of(matchCount: Int, matchBonus: Boolean): Rank {
+            return when {
+                matchCount == 6 -> FIRST
+                matchCount == 5 && matchBonus -> SECOND
+                matchCount == 5 -> THIRD
+                matchCount == 4 -> FOURTH
+                matchCount == 3 -> FIFTH
+                else -> NONE
+            }
+        }
+    }
 }
