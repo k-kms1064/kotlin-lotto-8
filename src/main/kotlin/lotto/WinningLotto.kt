f97ecd6742a0f9bbf2ce34c7e57fc67c7d3eee69
k-kms1064 <kms106418@gmail.com>
2025-10-30 23:39:21 +0900

    feat: WinningLotto match 함수 구현 (사용자 로또와 당첨 번호 비교)

--- src/main/kotlin/lotto/WinningLotto.kt
+++ src/main/kotlin/lotto/WinningLotto.kt
@@ -9,4 +9,10 @@ class WinningLotto(
         require(bonusNumber in 1..45) { "[ERROR] 보너스 번호는 1부터 45 사이여야 합니다." }
         require(!winningNumbers.getNumbers().contains(bonusNumber)) { "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다." }
     }
+
+    fun match(userLotto: Lotto): Rank {
+        val matchCount = userLotto.getNumbers().count { it in winningNumbers.getNumbers() }
+        val matchBonus = userLotto.getNumbers().contains(bonusNumber)
+        return Rank.of(matchCount, matchBonus)
+    }
 }
